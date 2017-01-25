6ee091d8872b0792ef1b6c01bfa2a9ed4e9541a1
Fred Ross <fred.ross@packetsled.com>
2017-01-24 23:40:10 -0800

    Set up tsconfig.json. Got observables and observers passing type checking, and a Label view.

--- src/allorryll.ts
+++ src/allorryll.ts
@@ -3,75 +3,112 @@
  *
  * This Source Code Form is subject to the terms of the
  * Mozilla Public License, v. 2.0. If a copy of the MPL was
- * not distributed with this file, You can 
+ * not distributed with this file, You can
  * obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/*
+ * The starting point of an MVC app is a model. All models are instances
+ * of Observable, that is, observers can register on them to be updated
+ * when the model changes.
+ */
 
 interface Observable {
-    addObserver,
-    deleteObserver,
-    notify
+    addObserver : (observer: Observer) => void,
+    deleteObserver : (observer: Observer) => void,
+    notify : () => void
 }
 
-let Observable = function(content) {
-    function() {
-	let observers = new Set();
-
-	this.addObserver = function(observer) {
-	    this.observers.add(observer);
-	};
-
-	this.deleteObserver = function(observer) {
-	    this.observers.delete(observer);
-	};
-
-	this.notify = function() {
-	    this.observers.forEach(function(observer) {
-		observer.update();
-	    });
-	};
-    }();
-
-    // TODO Add fields from content
+interface Observer {
+  update: () => void
 };
 
-interface View {
-    writeOver(el),
-    update(msg)
-}
+/* To create a model, call Observable on an object literal, e.g.,
+ *
+ *     let myModel = Observable({
+ *         name: "Boris"
+ *     });
+ */
+let Observable = function(subject: any): Observable {
+  let observers = new Set();
 
+	subject.addObserver = function(observer: Observer): void {
+	    observers.add(observer);
+	};
+
+	subject.deleteObserver = function(observer: Observer): void {
+	    observers.delete(observer);
+	};
+
+	subject.notify = function(): void {
+	    observers.forEach((observer: Observer) => {
+		    observer.update();
+	    });
+  };
+
+  return subject;
+};
+
+/*
+ * The other basic part of MVC is views, but to make views such as buttons
+ * and labels reusable, we parameterize their behavior with objects that we
+ * call controllers.
+ *
+ * A view is anything that can be written over an element on the DOM. We choose
+ * writing over instead of adding a child because it is uniformly more powerful
+ * and makes the code implementing complex views simpler.
+ *
+ * Types of view are classes implementing View. Each class has a corresponding
+ * interface defining the controller for that view.
+ */
+interface View extends Observer {
+    writeOver: (el: HTMLElement) => void,
+};
+
+
+/* Label - a simple span element containing text. */
 interface LabelController {
-    getText : function(Observable) -> string,
-}
+    getText: (o: Observable) => string,
+    getClass?: (o: Observable) => string
+};
 
-let Label = function(model : Observable, controller : LabelController) {
+class Label implements View {
+  model: Observable;
+  controller: LabelController;
+  span: HTMLElement;
+
+  constructor(model: Observable, controller: LabelController) {
     this.model = model;
     this.controller = controller;
     this.model.addObserver(this);
+  }
 
-    this.writeOver = function(el) {
-	this.span = document.createElement('span');
-	this.span.textContent = this.controller.getText(this.model);
-	if (this.controller.getClass) {
-	    this.span.setAttribute('class', this.controller.getClass());
-	}
-	el.parentNode.replaceChild(this.span, el);
-    };
+  writeOver(el : HTMLElement): void {
+    this.span = document.createElement('span');
+    this.span.textContent = this.controller.getText(this.model);
+    if (this.controller.getClass) {
+      this.span.setAttribute('class', this.controller.getClass(this.model));
+    }
+    if (el.parentNode) {
+      el.parentNode.replaceChild(this.span, el);
+    }
+  }
 
-    this.update = function() {
-	/*
-	  We don't want to do anything to the DOM if there are no
-	  changes needed, nor do we want users to lose whatever
-	  text they may have selected.
-	*/
-	var newText = this.controller.getText(model);
-	if (newText !== this.span.textContent) {
-	    this.span.textContent = newText;
-	}
-	if (this.controller.getClass &&
-	    this.controller.getClass(msg) !== this.span.getAttribute('class')) {
-	    this.span.setAttribute('class', this.controller.getClass(msg));
-	}
-    };
-}
+  update(): void {
+    /*
+  	  We don't want to do anything to the DOM if there are no
+  	  changes needed, nor do we want users to lose whatever
+  	  text they may have selected.
+  	*/
+  	let newText = this.controller.getText(this.model);
+  	if (newText !== this.span.textContent) {
+  	    this.span.textContent = newText;
+  	}
+  	if (this.controller.getClass) {
+        let newClass = this.controller.getClass(this.model);
+        if (newClass !== this.span.getAttribute('class')) {
+  	     this.span.setAttribute('class', this.controller.getClass(this.model));
+        }
+  	}
+  }
+};
